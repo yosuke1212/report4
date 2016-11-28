51561471a08abceb74fe94d924110c2a06d2820a
Yousuke Nakamura <e165717@ie.u-ryukyu.ac.jp>
2016-11-28 18:08:05 +0900

    ああああ

--- src/test/jp/ac/uryukyu/ie/e165717/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e165717/EnemyTest.java
@@ -1,22 +1,18 @@
 package jp.ac.uryukyu.ie.e165717;
-
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
-/**
- * Created by e165717 on 2016/11/28.
- */
 public class EnemyTest {
     @Test
-    public void attack() throws Exception {int heroHP = 10;
+    public void attack() throws Exception {
+        int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
-        enemy.dead = true;
+        enemy.setDead(true);
         for(int i=0; i<10; i++) {
             enemy.attack(hero); //乱数で0ダメージとなることもあるため、複数回実行してみる。
         }
-        assertEquals(heroHP, hero.hitPoint);
+        assertEquals(heroHP, hero.getHitPoint());
 
     }
 
